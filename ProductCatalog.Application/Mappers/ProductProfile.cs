a329eaa35bd3c0bbe15551580bf5522b626e559e
Ivan/Vano/Vanok/Vanya <94911526+MAZUTA89@users.noreply.github.com>
2025-12-05 05:59:25 +0300

    replace prjct

---
+++ ProductCatalog.Application/Mappers/ProductProfile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using AutoMapper.Configuration;
+using ProductCatalog.Application.DTO;
+using ProductCatalog.Application.DTOs;
+using ProductCatalog.Domain.Core.Entities;
+
+namespace ProductCatalog.Application.Mappers
+{
+    public class ProductProfile : Profile 
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDto>()
+                .ForMember(
+                d => d.Title,
+                src => src.MapFrom(s => s.Title))
+                .ForMember(
+                s => s.Description,
+                d => d.MapFrom(dto => dto.Description));
+
+            CreateMap<ProductImage, ProductImageDto>()
+                .ForMember(
+                d => d.FileName,
+                src => src.MapFrom(s => s.FileName));
+
+            CreateMap<Product, ProductCsvDto>()
+                .ForMember(
+                d => d.Id,
+                s => s.MapFrom(src => src.Id))
+                .ForMember(
+                d => d.Title,
+                s => s.MapFrom(src => src.Title))
+                .ForMember(
+                d => d.Description,
+                s => s.MapFrom(src => src.Description))
+                .ForMember(
+                d => d.ImagesAmount,
+                s => s.MapFrom(src => src.Images.Count));
+        }
+    }
+}
